987db0f562df73d76982b1352b429ec31815bbec
Taylor <258266864@qq.com>
2024-07-04 00:07:30 +0800

    Persistence and CLI

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mini_blockchain_go
+
+```bash
+$ go build -o blockchain_go
+
+$ ./blockchain_go printchain
+```
--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 func main() {
-	POW_sha256_hash_example()
+	bc := NewBlockchain()
+	defer bc.db.Close()
 
-	// bc := NewBlockchain()
-	// bc.AddBlock("Send 1 BTC to to Ivan")
-	// bc.AddBlock("Send 2 more BTC to Ivan")
-
-	// for _, block := range bc.blocks {
-	// 	fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-	// 	fmt.Printf("Data: %s\n", block.Data)
-	// 	fmt.Printf("Hash: %x\n", block.Hash)
-	// 	fmt.Println()
-	// 	pow := NewPOW(block)
-	// 	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-	// 	fmt.Println()
-	// }
+	cli := CLI{bc}
+	cli.Run()
 }
